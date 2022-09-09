75422060b5b52539620efcceb3099d82f3adb976
Bryan Saltos <brylo1.@outlook.com>
2022-09-09 16:15:26 +0400

    Mi primer commit

---
+++ js/data.js
@@ -0,0 +1,205 @@
+const  insectos = [
+    {
+        orden:'coleoptera',
+        familia:'scarabidae',
+        imagen:"./img/orden/coleoptera/Brentidae Limpio.JPG"
+    }
+    ,
+    {
+        orden:'coleoptera',
+        familia:'buprestidae',
+        imagen:"./img/orden/coleoptera/BuprestidaeZoomLimpio.png"
+    }
+    ,
+    {
+        orden:'coleoptera',
+        familia:'curculionidae',
+        imagen:"./img/orden/coleoptera/curculionidaeLimpio.png"
+    },
+    {
+        orden:'coleoptera',
+        familia:'carabidae',
+        imagen:"./img/orden/coleoptera/carabidaeZomLimpio.png"
+    },
+    {
+        orden:'coleoptera',
+        familia:'cerambycidae',
+        imagen:"./img/orden/coleoptera/CerambycidaeZoomLimpio.png"
+    },
+    {
+        orden:'coleoptera',
+        familia:'dysticidae',
+        imagen:"./img/orden/coleoptera/dysticidaeZoomLimpio.png"
+    },
+    {
+        orden:'coleoptera',
+        familia:'Elateridae',
+        imagen:"./img/orden/coleoptera/ElateridaeZoomLimpio.png"
+    },
+    {
+        orden:'coleoptera',
+        familia:'lampyridae',
+        imagen:"./img/orden/coleoptera/lampyridaeZoomLimpio.png"
+    },
+    {
+        orden:'coleoptera',
+        familia:'tenobrinidae',
+        imagen:"./img/orden/coleoptera/tenobrinidaeLimpio.jpg"
+    },
+    {
+        orden:'Mantodea',
+        familia:'Mantidae',
+        imagen:"./img/orden/mantodea/MantidaeLimpio.JPG"
+    },
+    {
+        orden:'dermaptera',
+        familia:'Blaberidae',
+        imagen:"./img/orden/dermaptera/BlaberidaeLimpia.png"
+    },
+    {
+        orden:'dermaptera',
+        familia:'Blattidae',
+        imagen:"./img/orden/dermaptera/BlattidaeLimpia.png"
+    },
+    {
+        orden:'dermaptera',
+        familia:'Blattodea',
+        imagen:"./img/orden/dermaptera/BlattodeaLimpia.jpg"
+    },
+    {
+        orden:'dyptera',
+        familia:'Asilidae',
+        imagen:"./img/orden/Dyptera/AsilidaeZoom Limpia.jpg"
+    },
+    {
+        orden:'dyptera',
+        familia:'Syrphidae',
+        imagen:"./img/orden/Dyptera/SyrphidaeLimpia.png"
+    },
+    {
+        orden:'hemiptera',
+        familia:'Belostommatidae',
+        imagen:"./img/orden/hemiptera/BelostommatidaeZoomLimpia.jpg"
+    },
+    {
+        orden:'hemiptera',
+        familia:'Cicadidae',
+        imagen:"./img/orden/hemiptera/CicadidaeZoomLimpia.jpg"
+    }
+    ,
+    {
+        orden:'hemiptera',
+        familia:'Coreidae',
+        imagen:"./img/orden/hemiptera/CoreidaeZoom Limpia.jpg"
+    },
+    {
+        orden:'hemiptera',
+        familia:'Pentatomidae',
+        imagen:"./img/orden/hemiptera/PentatomidaeZoom Limpia.jpg"
+    },
+    {
+        orden:'hemiptera',
+        familia:'Pyrrhocoridae',
+        imagen:"./img/orden/hemiptera/PyrrhocoridaeZoom Limpio.jpg"
+    },
+    {
+        orden:'hemiptera',
+        familia:'reduviidae',
+        imagen:"./img/orden/hemiptera/reduviidaeZoomLimpia.jpg"
+    },
+    {
+        orden:'hemiptera',
+        familia:'scutelleridae',
+        imagen:"./img/orden/hemiptera/scutelleridaeZoom Limpia.jpg"
+    },
+    {
+        orden:'hymemoptera',
+        familia:'Apidae',
+        imagen:"./img/orden/hymenoptera/ApidaeZoom Limpia.png"
+    },
+    {
+        orden:'hymemoptera',
+        familia:'Braconidae',
+        imagen:"./img/orden/hymenoptera/Braconidae ZoomLimpia.jpg"
+    },
+    {
+        orden:'hymemoptera',
+        familia:'formicidae',
+        imagen:"./img/orden/hymenoptera/formicidaeZoom Limpiop.jpg"
+    },
+    {
+        orden:'hymemoptera',
+        familia:'multilidae',
+        imagen:"./img/orden/hymenoptera/multilidaeZoom Limpio.jpg"
+    },
+    {
+        orden:'hymemoptera',
+        familia:'pompilidae',
+        imagen:"./img/orden/hymenoptera/pompilidaeZoomLimpio.jpg"
+    },
+    {
+        orden:'hymemoptera',
+        familia:'Scoliidae',
+        imagen:"./img/orden/hymenoptera/ScoliidaeZoom2 Limpia.jpg"
+    },
+    {
+        orden:'hymemoptera',
+        familia:'sphecidae',
+        imagen:"./img/orden/hymenoptera/sphecidaeZoom Limpio.jpg"
+    },
+    {
+        orden:'hymemoptera',
+        familia:'vespidae',
+        imagen:"./img/orden/hymenoptera/vespidaeZoom2 Limpio.jpg"
+    },
+    {
+        orden:'lepidoptera',
+        familia:'Saturnidae',
+        imagen:"./img/orden/lepidoptera/Saturnidaelimpia.jpg"
+    },
+    {
+        orden:'megaloptera',
+        familia:'corydalinae',
+        imagen:"./img/orden/megaloptera/MegalopteraLimpio.jpg"
+    },
+    {
+        orden:'neuroptera',
+        familia:'neuroptera',
+        imagen:"./img/orden/neuroptera/neuroptera Limpia.jpg"
+    },
+    {
+        orden:'odonata',
+        familia:'Cordulegastridae',
+        imagen:"./img/orden/Odonata/OdonataZoom2 Limpio.jpg"
+    },
+    {
+        orden:'Orthoptera',
+        familia:'Acridae',
+        imagen:"./img/orden/Orthoptera/Acridae Limpia.jpg"
+    },
+    {
+        orden:'Orthoptera',
+        familia:'Gryllidae',
+        imagen:"./img/orden/Orthoptera/Gryllidae Limpio.jpg"
+    },
+    {
+        orden:'Orthoptera',
+        familia:'Gryllotalpidae',
+        imagen:"./img/orden/Orthoptera/Gryllotalpidae Limpio.jpg"
+    },
+    {
+        orden:'Orthoptera',
+        familia:'Procospiidae',
+        imagen:"./img/orden/Orthoptera/Procospiidae Limpio.jpg"
+    },
+    {
+        orden:'Orthoptera',
+        familia:'Tettigonidae',
+        imagen:"./img/orden/Orthoptera/Tettigonidae Limpio.jpg"
+    }
+    
+    
+    
+    
+]
+export default  insectos
